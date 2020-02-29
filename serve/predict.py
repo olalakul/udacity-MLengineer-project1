bd96de9be7f07858cb96734c1a5186d478e0abc3
olalakul <olalakul@gmail.com>
2020-02-29 19:16:32 +0000

    completed successfully

--- serve/predict.py
+++ serve/predict.py
@@ -70,10 +70,7 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    #data_X = None
-    #data_len = None
-    data_X, data_len = convert_and_pad(model.word_dict, input_data)
-    
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -88,9 +85,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    #result = None
     out = model.forward(data)
-    #result = round(float(out.cpu().detach().numpy().decode('utf-8'))) # "'numpy.ndarray' object has no attribute 'decode'"
-    result = out.cpu().detach().numpy()#.round()
+    result = out.cpu().detach().numpy().round()
     
     return result
